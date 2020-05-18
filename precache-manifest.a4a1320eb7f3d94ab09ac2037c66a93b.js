898abb038ca60af3dcedda5efc76f9e85288155a
Igor Shtr <igor.shtr@gmail.com>
2020-05-18 10:27:59 +0300

    Updates

---
+++ precache-manifest.a4a1320eb7f3d94ab09ac2037c66a93b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6badc16e6578e272a31f2445d672bc",
+    "url": "/cucknorris/index.html"
+  },
+  {
+    "revision": "96717c3f0a4e82e9fd9d",
+    "url": "/cucknorris/static/css/main.e1560738.chunk.css"
+  },
+  {
+    "revision": "a01114bd1c9b60d637a8",
+    "url": "/cucknorris/static/js/2.cb5fa9f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cucknorris/static/js/2.cb5fa9f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96717c3f0a4e82e9fd9d",
+    "url": "/cucknorris/static/js/main.ffb38b13.chunk.js"
+  },
+  {
+    "revision": "699f08496811016939da",
+    "url": "/cucknorris/static/js/runtime-main.72121af4.js"
+  }
+]);
